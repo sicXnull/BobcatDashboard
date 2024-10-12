e70e1fcb8f88093cb48a6e41931e13a65a9ed761
sic <sicnull@gmail.com>
2024-10-11 20:04:56 -0400

    Update bt.sh

--- monitor-scripts/bt.sh
+++ monitor-scripts/bt.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 bobcat_ver=$(cat /var/dashboard/statuses/bobcat_ver)
-
-if [[ "$bobcat_ver" = "X1" ]]; then
-    gateway_config_path="/opt/gateway_config"
-fi
-
-if [[ "$bobcat_ver" = "X2" ]]; then
-    gateway_config_path="/opt/bobcat/gateway_config"
-fi
+gateway_config_path="/opt/gateway_config"
 
 data=$(sudo ${gateway_config_path}/bin/gateway_config advertise status)
 
