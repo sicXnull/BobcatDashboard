23be920df38bc6ea766e0a8c92ab8aa98b6940b9
sic <sicnull@gmail.com>
2024-10-15 19:50:12 -0400

    Update miner-update.sh

--- monitor-scripts/miner-update.sh
+++ monitor-scripts/miner-update.sh
@@ -13,15 +13,46 @@ docker pull $IMAGE_NAME
 # Get the latest image ID from the local Docker images
 LATEST_IMAGE_ID=$(docker images -q $IMAGE_NAME)
 
-if [ "$CURRENT_IMAGE_ID" == "$LATEST_IMAGE_ID" ]; then
-    echo "Miner Up to Date"
-else
-    echo "Updating Miner to Latest Version..."
-    
-    # Stop and remove the existing container only if the update is needed
-    docker stop $CONTAINER_NAME
-    docker rm $CONTAINER_NAME
+# Check if the container exists
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    # Container exists
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        # Container is running
+        if [ "$CURRENT_IMAGE_ID" != "$LATEST_IMAGE_ID" ]; then
+            echo "Updating Miner to Latest Version..."
+            
+            # Stop and remove the existing container
+            docker stop $CONTAINER_NAME
+            docker rm $CONTAINER_NAME
+            
+            # Spin up a new instance of the miner
+            docker run -d --privileged \
+                --name $CONTAINER_NAME \
+                -e GW_KEYPAIR=ecc://i2c-5:96?slot=0 \
+                -e GW_ONBOARDING=ecc://i2c-5:96?slot=15 \
+                -e RUST_BACKTRACE=1 \
+                -e GW_LISTEN=0.0.0.0:1680 \
+                --network host \
+                --restart unless-stopped \
+                --device /dev/i2c-5:/dev/i2c-5:rwm \
+                $IMAGE_NAME \
+                helium_gateway server
 
+            echo "Helium Miner has been updated and is now running."
+        else
+            echo "Miner is running and up to date."
+        fi
+    else
+        # Container exists but is not running
+        echo "Container exists but is not running. Starting the container..."
+        
+        # Spin up a new instance of the miner
+        docker start $CONTAINER_NAME
+    fi
+else
+    # Container does not exist
+    echo "Container does not exist. Starting a new instance..."
+    
     # Spin up a new instance of the miner
     docker run -d --privileged \
         --name $CONTAINER_NAME \
@@ -35,10 +66,5 @@ else
         $IMAGE_NAME \
         helium_gateway server
 
-    # Check if the container is running
-    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-        echo "Helium Miner is now running."
-    else
-        echo "Failed to start Helium Miner."
-    fi
+    echo "Helium Miner is now running."
 fi
