c253451d42403bc486b3055c52c45a6120014cb2
sic <sicnull@gmail.com>
2024-10-26 21:08:15 -0400

    Update service-pf.sh

--- monitor-scripts/service-pf.sh
+++ monitor-scripts/service-pf.sh
@@ -2,40 +2,42 @@
 
 bobcatVersion=$(cat /etc/bobcat-version | tr -d "\n")
 
-if [[ "$bobcatVersion" == 29* || "$bobcatVersion" == 285 ]]; then
-    
-    status=$(cat /var/dashboard/services/PF | tr -d "\n")
+case "$bobcatVersion" in
+    29* | 285)
+        status=$(cat /var/dashboard/services/PF | tr -d "\n")
 
-    if [[ $status == 'stop' ]]; then
-        docker stop pktfwd
-        echo 'disabled' > /var/dashboard/services/PF
-    elif [[ $status == 'start' ]]; then
-        docker start pktfwd
-        echo 'starting' > /var/dashboard/services/PF
-    elif [[ $status == 'starting' ]]; then
-        pid=$(docker ps -q --filter "name=pktfwd")
-        if [[ $pid ]]; then
-            echo 'running' > /var/dashboard/services/PF
+        if [[ $status == 'stop' ]]; then
+            docker stop pktfwd
+            echo 'disabled' > /var/dashboard/services/PF
+        elif [[ $status == 'start' ]]; then
+            docker start pktfwd
+            echo 'starting' > /var/dashboard/services/PF
+        elif [[ $status == 'starting' ]]; then
+            pid=$(docker ps -q --filter "name=pktfwd")
+            if [[ $pid ]]; then
+                echo 'running' > /var/dashboard/services/PF
+            fi
         fi
-    fi
+        ;;
 
-elif [[ "$bobcatVersion" == 280 ]]; then
-    
-    status=$(cat /var/dashboard/services/PF | tr -d "\n")
+    280)
+        status=$(cat /var/dashboard/services/PF | tr -d "\n")
 
-    if [[ $status == 'stop' ]]; then
-        docker stop pktfwd
-        echo 'disabled' > /var/dashboard/services/PF
-    elif [[ $status == 'start' ]]; then
-        docker start pktfwd
-        echo 'starting' > /var/dashboard/services/PF
-    elif [[ $status == 'starting' ]]; then
-        pid=$(docker ps -q --filter "name=pktfwd")
-        if [[ $pid ]]; then
-            echo 'running' > /var/dashboard/services/PF
+        if [[ $status == 'stop' ]]; then
+            systemctl stop lora-pkt-fwd.service
+            echo 'disabled' > /var/dashboard/services/PF
+        elif [[ $status == 'start' ]]; then
+            systemctl start lora-pkt-fwd.service
+            echo 'starting' > /var/dashboard/services/PF
+        elif [[ $status == 'starting' ]]; then
+            pid=$(pgrep lora_pkt_)
+            if [[ $pid ]]; then
+                echo 'running' > /var/dashboard/services/PF
+            fi
         fi
-    fi
-else
-    echo "Unsupported bobcat version: $bobcatVersion"
-fi
+        ;;
 
+    *)
+        echo "Unsupported bobcat version: $bobcatVersion"
+        ;;
+esac
